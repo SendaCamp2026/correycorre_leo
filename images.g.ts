546f6e02076d42873b3a1858bb4c5cec57637545
SendaCamp2026 <sendacamp2026@gmail.com>
2026-07-09 19:42:25 -0700

    Archivos iniciales del proyecto de MakeCode

---
+++ images.g.ts
@@ -0,0 +1,254 @@
+// Código generado automáticamente. No editar.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image11":
+            case "Enemigo1":return img`
+. . . . c c c c c c . . . . . . 
+. . . c 6 7 7 7 7 6 c . . . . . 
+. . c 7 7 7 7 7 7 7 7 c . . . . 
+. c 6 7 7 7 7 7 7 7 7 6 c . . . 
+. c 7 c 6 6 6 6 c 7 7 7 c . . . 
+. f 7 6 f 6 6 f 6 7 7 7 f . . . 
+. f 7 7 7 7 7 7 7 7 7 7 f . . . 
+. . f 7 7 7 7 6 c 7 7 6 f c . . 
+. . . f c c c c 7 7 6 f 7 7 c . 
+. . c 7 2 7 7 7 6 c f 7 7 7 7 c 
+. c 7 7 2 7 7 c f c 6 7 7 6 c c 
+c 1 1 1 1 7 6 f c c 6 6 6 c . . 
+f 1 1 1 1 1 6 6 c 6 6 6 6 f . . 
+f 6 1 1 1 1 1 6 6 6 6 6 c f . . 
+. f 6 1 1 1 1 1 1 6 6 6 f . . . 
+. . c c c c c c c c c f . . . . 
+`;
+            case "image12":
+            case "enemigo2":return img`
+. . . . . . f f f f . . . . . . 
+. . . . f f 1 1 1 1 f f . . . . 
+. . . f b 1 1 1 1 1 1 b f . . . 
+. . . f 1 1 1 1 1 1 1 1 f . . . 
+. . f d 1 1 1 1 1 1 1 1 d f . . 
+. . f d 1 1 1 1 1 1 1 1 d f . . 
+. . f d d d 1 1 1 1 d d d f . . 
+. . f b d b f d d f b d b f . . 
+. . f c d c f 1 1 f c d c f . . 
+. . . f b 1 1 1 1 1 1 b f . . . 
+. . f f f c d b 1 b d f f f f . 
+f c 1 1 1 c b f b f c 1 1 1 c f 
+f 1 b 1 b 1 f f f f 1 b 1 b 1 f 
+f b f b f f f f f f b f b f b f 
+. . . . . f f f f f f . . . . . 
+. . . . . . . f f f . . . . . . 
+`;
+            case "image10":
+            case "Obstaculo":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . e 2 . . . . . . . 
+. . . . . . e e 4 e . . . . . . 
+. . . . . . e 2 4 e . . . . . . 
+. . . . . c c c e e e . . . . . 
+. . . . e e 2 2 2 4 e e . . . . 
+. . c f f f c c e e f f e e . . 
+. c c c c e e 2 2 2 2 4 2 e e . 
+c c c c c c e e 2 2 2 4 2 2 e e 
+c c c c c c e e 2 2 2 2 4 2 e e 
+`;
+            case "image7":
+            case "Recompensa1":return img`
+....................
+....................
+....................
+....................
+....................
+....................
+.......22...22......
+......2322.2222.....
+......232222222.....
+......222222222.....
+.......22222b2......
+........222b2.......
+.........222........
+..........2.........
+....................
+....................
+....................
+....................
+....................
+....................
+`;
+            case "image8":
+            case "recompensa2":return img`
+. . . . . . . e c 7 . . . . . . 
+. . . . e e e c 7 7 e e . . . . 
+. . c e e e e c 7 e 2 2 e e . . 
+. c e e e e e c 6 e e 2 2 2 e . 
+. c e e e 2 e c c 2 4 5 4 2 e . 
+c e e e 2 2 2 2 2 2 4 5 5 2 2 e 
+c e e 2 2 2 2 2 2 2 2 4 4 2 2 e 
+c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+c e e 2 2 2 2 2 2 2 2 2 2 4 2 e 
+. e e e 2 2 2 2 2 2 2 2 2 4 e . 
+. 2 e e 2 2 2 2 2 2 2 2 4 2 e . 
+. . 2 e e 2 2 2 2 2 4 4 2 e . . 
+. . . 2 2 e e 4 4 4 2 e e . . . 
+. . . . . 2 2 e e e e . . . . . 
+`;
+            case "image9":
+            case "recompensa3":return img`
+. . . b b b . . 
+. . b 5 5 5 b . 
+. b 5 d 3 d 5 b 
+. b 5 1 5 3 5 b 
+. c d 1 5 3 5 c 
+. c d d 1 d 5 c 
+. . f d d d f . 
+. . . f f f . . 
+`;
+            case "image5":
+            case "Ayudante":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . b 5 5 b . . . 
+. . . . . . b b b b b b . . . . 
+. . . . . b b 5 5 5 5 5 b . . . 
+. b b b b b 5 5 5 5 5 5 5 b . . 
+. b d 5 b 5 5 5 5 5 5 5 5 b . . 
+. . b 5 5 b 5 d 1 f 5 d 4 f . . 
+. . b d 5 5 b 1 f f 5 4 4 c . . 
+b b d b 5 5 5 d f b 4 4 4 4 b . 
+b d d c d 5 5 b 5 4 4 4 4 4 4 b 
+c d d d c c b 5 5 5 5 5 5 5 b . 
+c b d d d d d 5 5 5 5 5 5 5 b . 
+. c d d d d d d 5 5 5 5 5 d b . 
+. . c b d d d d d 5 5 5 b b . . 
+. . . c c c c c c c c b b . . . 
+`;
+            case "image6":
+            case "Ayudante2":return img`
+. . . . . . . . . . . . . . . . 
+. . . . 4 4 4 . . . . 4 4 4 . . 
+. . . 4 5 5 5 e . . e 5 5 5 4 . 
+. . 4 5 5 5 5 5 e e 5 5 5 5 5 4 
+. . 4 5 5 4 4 5 5 5 5 4 4 5 5 4 
+. . e 5 4 4 5 5 5 5 5 5 4 4 5 e 
+. . . e e 5 5 5 5 5 5 5 5 e e . 
+. . . . e 5 f 5 5 5 5 f 5 e . . 
+. f f . f 5 5 5 4 4 5 5 5 f . . 
+. f 5 f f 6 5 5 f f 5 5 4 . . . 
+. f 5 5 f 4 6 6 6 6 6 6 f . . . 
+. . f 5 4 5 5 5 5 5 5 5 f . . . 
+. . . f f f 5 f 5 4 5 f . . . . 
+. . . . f f f f f f f . . . . . 
+`;
+            case "image3":
+            case "Fall":return img`
+. . . . . . . . . . . . . . 
+. f f f . f f f f f . . . . 
+f f f f f c c c c f f . . . 
+f f f f b c c c c c c f . . 
+f f f c 3 c c c c c c f . . 
+. f 3 3 c c c c c c c c f . 
+. f f f c c c c c 4 c c f . 
+. f f f f c c c 4 4 e f f . 
+. f f 4 4 f b f 4 4 e f f . 
+. . f 4 d 4 1 f d d f f . . 
+. . f f f 4 d d d d f . . . 
+. . . 4 d d e 3 3 3 f . . . 
+. . . e d d e 3 3 3 f . . . 
+. . . f e e f 6 6 6 f . . . 
+. . . . f f f f f f . . . . 
+. . . . . f f f . . . . . . 
+`;
+            case "image2":
+            case "Walking":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+f f . . f f f f f f f f f f f f 
+f 6 f . f 6 6 6 6 6 6 f 6 f 6 f 
+f 6 6 f f 6 6 6 2 6 6 6 6 6 6 f 
+. f 6 6 f 6 6 4 5 3 5 6 5 6 5 f 
+. . f 6 f 6 6 6 9 6 5 5 5 5 5 f 
+. . . f f f f f f f f f f f f f 
+. . . . . 6 6 . 6 6 . 6 6 . 6 6 
+. . . . . 6 6 . 6 6 . 6 6 . 6 6 
+. . . . . 6 6 . 6 6 . 6 6 . 6 6 
+`;
+            case "image4":
+            case "Idle":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . f f f f f f f . . . . 
+. . . . . f 6 6 6 6 6 f . . . . 
+. f f f f f f f f f f f f f f f 
+. f 6 6 f 6 6 6 6 6 6 6 f 6 6 f 
+. f 6 6 f 6 6 6 6 6 6 6 f 6 6 f 
+. f 6 6 f 6 f 6 6 6 f 6 f 6 6 f 
+. f 6 6 f 6 6 6 6 6 6 6 f 6 6 f 
+. f 6 6 f 6 5 6 5 6 5 6 f 6 6 f 
+. f f f f 6 5 5 5 5 5 6 f f f f 
+. . . f 6 6 6 6 6 6 6 6 6 f . . 
+. . . f 6 6 6 6 6 6 6 6 6 f . . 
+. . . f f f f f f f f f f f f . 
+. . . f f 6 f f 6 f 6 f f 6 f . 
+. . . f f 6 f f 6 f 6 f f 6 f . 
+. . . f f f f f f f f f f f f . 
+`;
+            case "image1":
+            case "Jump":return img`
+. f f f f f f f f f f f f f f f 
+f f 6 f 6 6 6 6 6 6 6 6 6 f 6 f 
+f f 6 f 6 6 6 6 6 6 6 6 6 f 6 f 
+f f 6 f 6 6 f 6 6 6 f 6 6 f 6 f 
+. f f f 6 6 6 6 6 6 6 6 6 f f f 
+9 f f f 6 6 6 6 6 6 6 6 6 f . . 
+9 9 f f 6 6 5 6 5 6 5 6 6 f . . 
+9 9 f f 6 6 5 5 5 5 5 6 6 f 9 9 
+9 . f f 6 6 6 6 6 6 6 6 6 f . 9 
+. . f f f f f f f f f f f f 9 9 
+. 9 . f 6 f f 6 f f 6 f 6 f . . 
+. 9 . f 6 f f 6 f f 6 f 6 f . . 
+9 . f f 6 f f 6 f f 6 f 6 f 9 9 
+9 9 f f f f f f f f f f f f . 9 
+. 9 9 9 f . . . f 9 9 . 9 9 . 9 
+. . 9 . . . . . . 9 9 . . 9 . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Código generado automáticamente. No editar.
---
+++ main.ts
@@ -0,0 +1,145 @@
+namespace SpriteKind {
+    export const obstaculo = SpriteKind.create()
+    export const Vida = SpriteKind.create()
+    export const Puntos = SpriteKind.create()
+    export const Ayudante = SpriteKind.create()
+}
+scene.onHitWall(SpriteKind.Player, function (sprite, location) {
+    if (sprite.isHittingTile(CollisionDirection.Bottom)) {
+        cantidadSaltos = 0
+    }
+})
+function crearObstaculos () {
+    for (let valor of tiles.getTilesByType(assets.tile`obstaculo1`)) {
+        mySprite7 = sprites.create(assets.image`Obstaculo`, SpriteKind.obstaculo)
+        tiles.placeOnTile(mySprite7, valor)
+        tiles.setTileAt(valor, assets.tile`baseTransparency16`)
+    }
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (cantidadSaltos < MaxSalto) {
+        cantidadSaltos += 1
+        mySprite.vy = -200
+    }
+})
+function SpawnEnemigo () {
+    if (nivelActual == 1) {
+        crearEnemigo("enemigo1", 23, 2, 0, 0, 3)
+        crearEnemigo("enemigo2", 7, 7, -30, 0, 2)
+    }
+}
+function configurarPersonaje () {
+    mySprite = sprites.create(assets.image`Idle`, SpriteKind.Player)
+    mySprite.ay = 300
+    controller.moveSprite(mySprite, velocidadX, 0)
+    scene.cameraFollowSprite(mySprite)
+    info.setLife(3)
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Puntos, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite)
+    info.changeScoreBy(1)
+})
+function crearEnemigo (tipo: string, columna: number, fila: number, velocidad_X: number, velocidad_y: number, vida: number) {
+    if (tipo == "enemigo1") {
+        mySprite8 = sprites.create(assets.image`Enemigo1`, SpriteKind.Enemy)
+        sprites.setDataString(mySprite8, "tipo", "enemigo1")
+        tiles.placeOnTile(mySprite8, tiles.getTileLocation(columna, fila))
+        mySprite8.setVelocity(velocidad_X, velocidad_y)
+    } else if (tipo == "enemigo2") {
+        mySprite9 = sprites.create(assets.image`enemigo2`, SpriteKind.Enemy)
+        sprites.setDataString(mySprite9, "tipo", "enemigo2")
+        tiles.placeOnTile(mySprite9, tiles.getTileLocation(columna, fila))
+        mySprite9.setVelocity(velocidad_X, velocidad_y)
+        mySprite9.setBounceOnWall(true)
+    }
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite)
+    velocidadX = 200
+    controller.moveSprite(mySprite, velocidadX, 0)
+    timer.after(2000, function () {
+        velocidadX = 100
+        controller.moveSprite(mySprite, velocidadX, 0)
+    })
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.obstaculo, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    scene.cameraShake(4, 500)
+    sprite.setFlag(SpriteFlag.GhostThroughSprites, true)
+    timer.after(500, function () {
+        sprite.setFlag(SpriteFlag.GhostThroughSprites, false)
+    })
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Vida, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite)
+    info.changeLifeBy(1)
+})
+function crearNivel () {
+    if (nivelActual == 1) {
+        tiles.setCurrentTilemap(tilemap`nivel1`)
+        tiles.placeOnTile(mySprite, tiles.getTileLocation(1, 7))
+    }
+    crearRecompensas()
+    crearObstaculos()
+    SpawnEnemigo()
+}
+function crearRecompensas () {
+    for (let valor of tiles.getTilesByType(assets.tile`recompensa1`)) {
+        mySprite4 = sprites.create(assets.image`Recompensa1`, SpriteKind.Vida)
+        tiles.placeOnTile(mySprite4, valor)
+        tiles.setTileAt(valor, assets.tile`baseTransparency16`)
+    }
+    for (let valor of tiles.getTilesByType(assets.tile`recompensa2`)) {
+        mySprite5 = sprites.create(assets.image`recompensa2`, SpriteKind.Food)
+        tiles.placeOnTile(mySprite5, valor)
+        tiles.setTileAt(valor, assets.tile`baseTransparency16`)
+    }
+    for (let valor of tiles.getTilesByType(assets.tile`recompensa3`)) {
+        mySprite6 = sprites.create(assets.image`recompensa3`, SpriteKind.Puntos)
+        tiles.placeOnTile(mySprite6, valor)
+        tiles.setTileAt(valor, assets.tile`baseTransparency16`)
+    }
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite)
+    info.changeLifeBy(-1)
+    scene.cameraShake(4, 500)
+})
+let direccion = 0
+let mySprite6: Sprite = null
+let mySprite5: Sprite = null
+let mySprite4: Sprite = null
+let mySprite9: Sprite = null
+let mySprite8: Sprite = null
+let mySprite: Sprite = null
+let mySprite7: Sprite = null
+let cantidadSaltos = 0
+let velocidadX = 0
+let MaxSalto = 0
+let nivelActual = 0
+scene.setBackgroundColor(9)
+music.setVolume(255)
+nivelActual = 1
+MaxSalto = 2
+velocidadX = 100
+configurarPersonaje()
+crearNivel()
+game.onUpdate(function () {
+    mySprite.setImage(assets.image`Walking`)
+    direccion = 1
+    if (mySprite.vx == 0) {
+        mySprite.setImage(assets.image`Idle`)
+    }
+    if (mySprite.vy < 0) {
+        mySprite.setImage(assets.image`Jump`)
+    } else if (mySprite.vy > 0) {
+        mySprite.setImage(assets.image`Idle`)
+    } else if (mySprite.vx < 0) {
+        mySprite.setImage(assets.image`Walking`)
+    }
+    if (mySprite.vx < 0) {
+        direccion = -1
+        mySprite.image.flipX()
+        mySprite.setImage(mySprite.image)
+    }
+})
